1be583ec68361db9834c910d0a769342f029f36e
Uranite <62639703+Uranite@users.noreply.github.com>
2026-03-31 11:50:44 +0700

    Update 40-gas-preprocessor.ps1

--- deps.ffmpeg/40-gas-preprocessor.ps1
+++ deps.ffmpeg/40-gas-preprocessor.ps1
@@ -10,3 +10,13 @@ function Setup {
     Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
 }
 
+function Patch {
+    Log-Information "Patch (${Target})"
+    Set-Location $Path
+
+    $gas = Get-Content gas-preprocessor.pl -Raw
+    $gas = $gas -replace '@preprocess_c_cmd = grep ! /\^-O/, @preprocess_c_cmd;', '@preprocess_c_cmd = grep ! /^-O/, @preprocess_c_cmd; @preprocess_c_cmd = grep ! /^-flto/, @preprocess_c_cmd; @preprocess_c_cmd = grep ! /^\/clang:/, @preprocess_c_cmd; @preprocess_c_cmd = grep ! /^-march=/, @preprocess_c_cmd;'
+    $gas = $gas -replace '@gcc_cmd = grep ! /\^-O/, @gcc_cmd;', '@gcc_cmd = grep ! /^-O/, @gcc_cmd; @gcc_cmd = grep ! /^-flto/, @gcc_cmd; @gcc_cmd = grep ! /^\/clang:/, @gcc_cmd; @gcc_cmd = grep ! /^-march=/, @gcc_cmd;'
+    Set-Content -Path gas-preprocessor.pl -Value $gas -NoNewline
+}
+

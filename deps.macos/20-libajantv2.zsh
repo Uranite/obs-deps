c60e0cdfe4a84f9622d18bb8f60cb3d0c1590b7c
Uranite <62639703+Uranite@users.noreply.github.com>
2026-03-31 17:02:21 +0700

    a

--- deps.macos/20-libajantv2.zsh
+++ deps.macos/20-libajantv2.zsh
@@ -5,6 +5,10 @@ local name='ntv2'
 local version='2d7636d86b6180bb4c5075fea040b1b812cc8b57'
 local url='https://github.com/aja-video/libajantv2.git'
 local hash='2d7636d86b6180bb4c5075fea040b1b812cc8b57'
+local -a patches=(
+  "* ${0:a:h}/patches/ajantv2/0001-install-m31-headers.patch \
+    D77DCCB550A1E9C1522ABEAD997C479065ECCCD251393BFF5CBF3B7BA6E222CB"
+)
 
 ## Dependency Overrides
 local -i shared_libs=0
@@ -13,6 +17,23 @@ local -i shared_libs=0
 setup() {
   log_info "Setup (%F{3}${target}%f)"
   setup_dep ${url} ${hash}
+}
+
+patch() {
+  autoload -Uz apply_patch
+
+  log_info "Patch (%F{3}${target}%f)"
+  cd ${dir}
+
+  local patch
+  local _target
+  local _url
+  local _hash
+  for patch (${patches}) {
+    read _target _url _hash <<< "${patch}"
+
+    if [[ ${target%%-*} == ${~_target} ]] apply_patch ${_url} ${_hash}
+  }
 }
 
 clean() {
